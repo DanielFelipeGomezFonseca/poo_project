0a4a2b70d292bc17dbc5f1f15d14d757eb048bc3
josedddd <144506601+josedddd@users.noreply.github.com>
2025-06-14 13:05:50 -0500

    Update project.py

--- project.py
+++ project.py
@@ -2,26 +2,45 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-url = "https://www.panamericana.com.co/audifonos-tipo-diadema-bass-13-686721/p"
-headers = {
-    "User-Agent": "Mozilla/5.0"
-}
 
-response = requests.get(url, headers=headers)
+class Webscrapper():
+    def __init__(self,name):
+        self.name=name
+    
+    def buscar_json(self):
+        url = "https://www.panamericana.com.co/audifonos-tipo-diadema-bass-13-686721/p"
+        headers = {
+        "User-Agent": "Mozilla/5.0"
+            }
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, "html.parser")
+        response = requests.get(url, headers=headers)
 
-    # Encuentra TODOS los bloques JSON en el HTML
-    scripts = soup.find_all("script", type="application/ld+json")
+        if response.status_code == 200:
+         soup = BeautifulSoup(response.text, "html.parser")
 
-    for script in scripts:
-        try:
-            data = json.loads(script.string)
-            # Solo procesamos si es un producto
-            if data.get("@type") == "Product":
-                print(data)
-        except (json.JSONDecodeError, TypeError):
-            continue  # Si el bloque no es un JSON válido, lo ignoramos
-else:
-    print(f"❌ Error al cargar la página. Código: {response.status_code}")
+         # Encuentra TODOS los bloques JSON en el HTML
+        scripts = soup.find_all("script", type="application/ld+json")
+
+        for script in scripts:
+             try:
+                 self.data = json.loads(script.string)
+                 # Solo procesamos si es un producto
+                 if self.data.get("@type") == "Product":
+                    pass
+             except (json.JSONDecodeError, TypeError):
+              continue  # Si el bloque no es un JSON válido, lo ignoramos
+        else:
+            print(f"❌ Error al cargar la página. Código: {response.status_code}")
+
+
+    def product_name(self):
+       names=[]
+       keys=self.data.keys()
+       for x in keys:
+          if x=="name":
+             names.append(self.data[x])
+       return names
+
+Web1=Webscrapper("audifonos")
+Web1.buscar_json()
+print(Web1.product_name())
