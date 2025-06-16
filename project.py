56e4a55d8c4683b37e978e800e33bc605e359632
josedddd <144506601+josedddd@users.noreply.github.com>
2025-06-15 21:54:32 -0500

    Update project.py

--- project.py
+++ project.py
@@ -2,38 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+class WebScrapper:
+    def __init__(self,object):
+       self._object = object
 
-class Webscrapper():
-    def __init__(self,name):
-        self.name=name
+    def parsear_json(self):
+       pass
+
+    def buscar_nombre(self):
+       pass
+
+class PanamericanaScrapper(WebScrapper):
+    def __init__(self,object):
+        super().__init__(object)
     
-    def buscar_json(self):
-        url = "https://www.panamericana.com.co/audifonos-tipo-diadema-bass-13-686721/p"
-        headers = {
-        "User-Agent": "Mozilla/5.0"
-            }
-
-        response = requests.get(url, headers=headers)
-
-        if response.status_code == 200:
-         soup = BeautifulSoup(response.text, "html.parser")
-
-         # Encuentra TODOS los bloques JSON en el HTML
+    def parsear_json(self):
+        url = "https://www.panamericana.com.co/audifonos-tipo-diadema-bass-13-686721/p"  
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
         scripts = soup.find_all("script", type="application/ld+json")
-
         for script in scripts:
-             try:
-                 self.data = json.loads(script.string)
-                 # Solo procesamos si es un producto
-                 if self.data.get("@type") == "Product":
-                    pass
-             except (json.JSONDecodeError, TypeError):
-              continue  # Si el bloque no es un JSON válido, lo ignoramos
-        else:
-            print(f"❌ Error al cargar la página. Código: {response.status_code}")
+            
+            self.data = json.loads(script.string)
 
+        print(self.data)
 
-    def product_name(self):
+    def buscar_nombre(self):
        names=[]
        keys=self.data.keys()
        for x in keys:
@@ -41,6 +35,6 @@ class Webscrapper():
              names.append(self.data[x])
        return names
 
-Web1=Webscrapper("audifonos")
-Web1.buscar_json()
-print(Web1.product_name())
+Web1=PanamericanaScrapper("audifonos")
+Web1.parsear_json()
+print(Web1.buscar_nombre())
